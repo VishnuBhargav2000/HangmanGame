165bc747fb082bd39f1b622d446fe74de23cefe9
VishnuBhargav2000 <40735102+VishnuBhargav2000@users.noreply.github.com>
2019-10-13 01:48:33 +0530

    added start screen and background image + heavily refined code

--- core.py
+++ core.py
@@ -34,6 +34,7 @@ def display_secret_word(secret_word1):
         posx += 35
 
 
+# noinspection PyShadowingNames
 class Button(object):
     # a button class that eases up the button creation
     # takes color(hex values) x and y co-ordinates, width, height and the text
@@ -70,66 +71,79 @@ class Button(object):
 
 
 def initialize_game():
-    posx = 80   # starting position of buttons
-    posy = 30
     win.blit(bg, (0, 0))
     global image, tries, secret_word, category, letter_buttons, secret_word_buttons, correct_letters_guessed
     letter_buttons = []
+
     # setting up the buttons
+    posx = 80   # starting position of buttons
+    posy = 30
     for i in range(1, 27):
         posx += 40
         if i == 14:
             posy += 50
             posx = 120
         letter_buttons.append(Button((255, 255, 255), posx, posy, 30, 30, button_values[i]))
+    # selecting a random secret word and category its from
     secret_word, category = vocab.choose_word()
     print(secret_word)
+
+    # resetting the tries user has
     tries = 10
+
+    # resetting the image to welcome image
     image = hangman_images["idle"]
+
+    # resetting the correct letters guessed and secret word buttons that will be displayed on the screen
     secret_word_buttons = []
     correct_letters_guessed = []
+
+    # resetting the text displayed on the game window
     display_secret_word(secret_word)
     category_button.update_text("category : " + category)
-    display_secret_word(secret_word)
     welcome_player.update_text("welcome, " + str(name))
 
 
 def redraw_game_window():
-    pygame.display.flip()
+    pygame.display.flip()   # updates the display
 
-    for obj in letter_buttons:
+    for obj in letter_buttons:  # displays the letter buttons on the screen
         if obj.guessed:
-            pass
+            pass    # if the letter is already guessed , it will not be displayed
         else:
             obj.draw(win)
 
     for but in secret_word_buttons:
-        but.draw(win)
+        but.draw(win)   # to display the secret word on the screen
 
     pygame.draw.rect(win, (255, 255, 255), [400, 150, 300, 300])
     tries_left_button.update_text("tries  left : " + str(tries) + "  ")
-    tries_left_button.draw(win)
+    if tries != -1:
+        tries_left_button.draw(win)
     category_button.draw(win)
     win.blit(image, (400, 150))
     welcome_player.draw(win)
 
 
 def end_game():
+    # quits the game
     pygame.quit()
 
 
+# noinspection PyShadowingNames
 def start_menu():
     global start, name
     posx = 80
-    posy = 120
+    posy = 170
     for i in range(1, 27):
         posx += 40
         if i == 14:
             posy += 50
             posx = 120
         letter_buttons.append(Button((255, 255, 255), posx, posy, 30, 30, button_values[i]))
-    done = Button((179, 220, 216), posx-100, posy+100, 60, 30, "DONE")
-    display_name = Button((255, 255, 255), posx-400, posy+100, 200, 30, " player name : ")
+    done = Button((179, 220, 216), posx-150, posy+100, 60, 30, "DONE")
+    display_name = Button((255, 255, 255), posx-400, posy+60, 200, 30, " player name : ")
+    bg = pygame.image.load("assets/start_screen.png")
 
     def redraw_window():
         pygame.display.flip()
@@ -142,6 +156,7 @@ def start_menu():
         done.draw(win)
         display_name.update_text(" player name : " + str(name))
         display_name.draw(win)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -158,8 +173,8 @@ def start_menu():
             for button in letter_buttons:
                 if button.is_over(pygame.mouse.get_pos()):
                     if event.type == pygame.MOUSEBUTTONDOWN:
-                            letter = button_values[letter_buttons.index(button)+1]
-                            name += letter
+                        letter = button_values[letter_buttons.index(button)+1]
+                        name += letter
             redraw_window()
 
 
@@ -167,16 +182,20 @@ def start_menu():
 button_values = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l',
                  13: 'm', 14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w',
                  24: 'x', 25: 'y', 26: 'z'}
+
 # list, when  filled up will contain all the buttons used for giving inputs
 letter_buttons = []
+
 # list, when  filled up will contain all the buttons used for showing the output
 secret_word_buttons = []
+
 # list that stores the letters guessed by the user that do exist in the word
 correct_letters_guessed = []
 
 # run == True >> game runs;     run == False >> game doesnt run
 run, start = True, True
 name = ""
+
 # based upon the difficulty the tries or guesses left is stored
 tries = 10
 
@@ -186,7 +205,7 @@ secret_word, category = vocab.choose_word()
 # button initialisation
 tries_left_button = Button((255, 255, 255), 20, 200, 200, 30, "tries  left : " + str(tries) + "  ")
 category_button = Button((255, 255, 255), 20, 150, 200, 30, "category : " + category)
-welcome_player = Button((255, 255, 255), 20, 200, 200, 30, "welcome, " + name)
+welcome_player = Button((255, 255, 255), 20, 120, 200, 30, "welcome, " + name)
 
 # starting the game
 # initialize_game()
@@ -243,8 +262,6 @@ while run:
         redraw_game_window()
 
     redraw_game_window()    # for showing the winning image
-    pygame.time.wait(5000)
+    pygame.time.wait(3000)
     initialize_game()
 
-
-pygame.quit()   # quits the game
